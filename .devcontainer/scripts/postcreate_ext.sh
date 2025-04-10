f4f8663f5967cac4da9253ec1e6d28b12d7f54fe
Juergen Schmid <15319906+hacki11@users.noreply.github.com>
2025-04-10 22:20:52 +0200

    add init script for testsetup

---
+++ .devcontainer/scripts/postcreate_ext.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+iob object set system.adapter.mhi-wfrac.0 native.interval=10
+iob object set system.adapter.mhi-wfrac.0 native.devices=[{\"name\":\"Device1\"},{\"name\":\"Device2\"},{\"name\":\"Device3\"}]
+
+iob object set system.adapter.mhi-wfrac.0 native.devices[0].ip=192.168.1.41
+iob object set system.adapter.mhi-wfrac.0 native.devices[0].enabled=true
+
+iob object set system.adapter.mhi-wfrac.0 native.devices[1].ip=192.168.1.42
+iob object set system.adapter.mhi-wfrac.0 native.devices[1].enabled=true
+
+iob object set system.adapter.mhi-wfrac.0 native.devices[2].ip=192.168.1.222
+iob object set system.adapter.mhi-wfrac.0 native.devices[2].enabled=false

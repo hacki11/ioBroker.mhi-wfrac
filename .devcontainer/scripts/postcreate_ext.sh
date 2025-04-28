3e137bcd0df11fd62444d72670daa5a765cfff9e
Juergen Schmid <15319906+hacki11@users.noreply.github.com>
2025-04-28 21:33:46 +0200

    update dev container config

--- .devcontainer/scripts/postcreate_ext.sh
+++ .devcontainer/scripts/postcreate_ext.sh
@@ -9,7 +9,7 @@ iob object set system.adapter.mhi-wfrac.0 native.devices[0].ip=192.168.1.41
 iob object set system.adapter.mhi-wfrac.0 native.devices[0].enabled=true
 
 iob object set system.adapter.mhi-wfrac.0 native.devices[1].ip=192.168.1.42
-iob object set system.adapter.mhi-wfrac.0 native.devices[1].enabled=true
+iob object set system.adapter.mhi-wfrac.0 native.devices[1].enabled=false
 
 iob object set system.adapter.mhi-wfrac.0 native.devices[2].ip=192.168.1.43
 iob object set system.adapter.mhi-wfrac.0 native.devices[2].enabled=true
